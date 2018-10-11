9a8ec11da8ee31ab5703645c65d40877bb379e1e
HyebinKim <qls5191@kaist.ac.kr>
2018-10-11 15:28:08 +0900

    mouse point
    1st coding

---
+++ CADapp_Unity/Assets/Scripts/State.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class State : MonoBehaviour {
+
+    public Text obj;
+    private Vector3 Mouse;
+    private bool state;
+
+
+    // Use this for initialization
+    void Start () {
+        obj.text = "off";
+        state = false;
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            state = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            state = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            state = false;
+        }
+
+
+        if (state == true)
+        {
+            Mouse = Input.mousePosition;
+            Mouse.z = 0;
+            obj.text = "x=" + Mouse.x + ", y=" + Mouse.y +", z=" + Mouse.z + "\n";
+        }
+        else
+        {
+            obj.text = "off";
+        }
+
+    }
+}
---
+++ CADapp_Unity/Assets/Scripts/drawing_line.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class drawing_line : MonoBehaviour {
+
+    public Text obj2;
+    private Vector2 Mouse;
+    private bool state;
+
+    Vector3 start = new Vector3(100, 100, 0);
+    Vector3 end = new Vector3(-100, -100, 0);
+
+    LineRenderer lr;
+
+    // Use this for initialization
+    void Start () {
+        state = false;
+        lr = GetComponent<LineRenderer>();
+        SetLine(start, end);
+        obj2.text = "off";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            state = true;
+            Mouse = Input.mousePosition;
+            start.x = Mouse.x;
+            start.y = Mouse.y;
+            //SetLine(start, start);
+            obj2.text = "start:("+start.x+","+start.y+"), end:(" + end.x + "," + end.y + ")";
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Mouse = Input.mousePosition;
+            end.x = Mouse.x;
+            end.y = Mouse.y;
+            //SetLine(start, end);
+            obj2.text = "start:(" + start.x + "," + start.y + "), end:(" + end.x + "," + end.y + ")";
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            state = false;
+            obj2.text = "start:(" + start.x + "," + start.y + "), end:(" + end.x + "," + end.y + ")";
+            //SetLine(start, end);
+        }
+
+
+    }
+
+    void SetLine(Vector3 start, Vector3 end)
+    {
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        lr.SetWidth(0.1f, 0.1f);
+    }
+}
