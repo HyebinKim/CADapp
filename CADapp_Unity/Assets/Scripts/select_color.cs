c8c14ebc2cb13081b79974ccb28e8aa607283650
HyebinKim <qls5191@kaist.ac.kr>
2018-10-23 22:38:39 +0900

    make planes
    -plane
    -click event

---
+++ CADapp_Unity/Assets/Scripts/select_color.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class plane_select : MonoBehaviour {
+
+    GameObject m_plane;
+    Text m_Text;
+    int count;
+
+    void Start()
+    {
+        count = 0;
+        m_plane = GetComponent<GameObject>();
+        m_Text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            m_Text.color = Color.yellow;
+        }
+        else
+        {
+            m_Text.color = Color.white;
+        }
+    }
+}
