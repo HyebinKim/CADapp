9cea0a3d5b4b146389803bf9883ef64dea4b5028
HyebinKim <qls5191@kaist.ac.kr>
2018-10-25 21:56:04 +0900

    draw_rec
    11

---
+++ CADapp_Unity/Assets/Scripts/UItoWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UItoWorld : MonoBehaviour {
+
+    public Text mText;
+    Vector3 wposition;
+    Vector2 mPosition;
+
+	// Use this for initialization
+	void Start () {
+        mText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mPosition = Input.mousePosition;
+        wposition = new Vector3(mPosition.x, mPosition.y, 0);
+        mText.text = "x=" + wposition.x + ", y=" + wposition.y + ", z= " + wposition.z;
+    }
+}
---
+++ CADapp_Unity/Assets/Scripts/move_xy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_xy : MonoBehaviour {
+
+    private float speed = 0.5f;
+    Vector3 refPos;
+    Vector3 targetPos;
+    Quaternion targetRot;
+    float moveSpeed = 0.05f;
+
+    //GameObject xy;
+    //GameObject yz;
+    //GameObject zx;
+
+    // Use this for initialization
+    void Start () {
+        refPos = new Vector3(15f, 15f, 15f);
+        targetPos = new Vector3(0f, 0f, 15f);
+        targetRot = new Quaternion(0, 90, 0, 1);
+        //xy.GetComponent<GameObject>();
+        //yz.GetComponent<GameObject>();
+        //zx.GetComponent<GameObject>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            while (transform.position != targetPos)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, moveSpeed);
+            }
+
+            //yz.active = false;
+            //zx.active = false;
+
+            //Camera.main.transform.position = targetPos;
+            //Camera.main.transform.rotation = targetRot;
+        }
+
+
+    }
+}
